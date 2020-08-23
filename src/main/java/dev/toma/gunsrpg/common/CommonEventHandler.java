726279024c02f89eacdda903dbd15f3e436aeac9
Toma <42510241+Toma1O6@users.noreply.github.com>
2020-08-23 12:14:57 +0200

    1.0.3, final

--- src/main/java/dev/toma/gunsrpg/common/CommonEventHandler.java
+++ src/main/java/dev/toma/gunsrpg/common/CommonEventHandler.java
@@ -157,14 +157,14 @@ public class CommonEventHandler {
         ForgeVersion.CheckResult result = ForgeVersion.getResult(Loader.instance().activeModContainer());
         switch (result.status) {
             case OUTDATED: case BETA_OUTDATED: {
-                TextComponentString textComponent = new TextComponentString(getMessageLogo() + TextFormatting.GREEN + "Your mod is " + TextFormatting.RED + "outdated" + TextFormatting.GREEN + ". Click " + TextFormatting.BOLD + "HERE" + TextFormatting.RESET.toString() + TextFormatting.GREEN + " to get latest update");
+                TextComponentString textComponent = new TextComponentString(getMessageLogo() + TextFormatting.GREEN + " Your mod is " + TextFormatting.DARK_RED + "outdated" + TextFormatting.GREEN + ". Click " + TextFormatting.BOLD + "HERE" + TextFormatting.RESET.toString() + TextFormatting.GREEN + " to get latest update");
                 String url = "https://www.curseforge.com/minecraft/mc-mods";
                 textComponent.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
                 event.player.sendMessage(textComponent);
                 break;
             }
             case UP_TO_DATE:
-                event.player.sendMessage(new TextComponentString(getMessageLogo() + TextFormatting.GREEN + "Your mod is up to date, enjoy!"));
+                event.player.sendMessage(new TextComponentString(getMessageLogo() + TextFormatting.GREEN + " Your mod is up to date, enjoy!"));
                 break;
         }
     }
