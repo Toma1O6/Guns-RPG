f79609c12e00d56116844a97a752579e052d3b1f
Toma <novotny.tom96@gmail.com>
2021-08-10 00:32:41 +0200

    Delete old animation system

--- src/main/java/dev/toma/gunsrpg/client/animation/impl/IAnimator.java
+++
@@ -1,9 +0,0 @@
-package dev.toma.gunsrpg.client.animation.impl;
-
-import com.mojang.blaze3d.matrix.MatrixStack;
-
-@Deprecated
-@FunctionalInterface
-public interface IAnimator {
-    void animate(MatrixStack stack, float interpolatedValue);
-}
