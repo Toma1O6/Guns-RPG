1947ab30d89ff1d3e046bdd945efcfb4199a13a1
Toma <novotny.tom96@gmail.com>
2021-11-13 14:11:29 +0100

    more animations

--- src/main/java/dev/toma/gunsrpg/common/item/guns/R45Item.java
+++ src/main/java/dev/toma/gunsrpg/common/item/guns/R45Item.java
@@ -1,9 +1,13 @@
 package dev.toma.gunsrpg.common.item.guns;
 
 import dev.toma.gunsrpg.GunsRPG;
+import dev.toma.gunsrpg.api.common.IReloadManager;
 import dev.toma.gunsrpg.client.render.RenderConfigs;
 import dev.toma.gunsrpg.client.render.item.R45Renderer;
+import dev.toma.gunsrpg.common.attribute.IAttributeProvider;
 import dev.toma.gunsrpg.common.init.Skills;
+import dev.toma.gunsrpg.common.item.guns.ammo.AmmoMaterials;
+import dev.toma.gunsrpg.common.item.guns.reload.ReloadManagers;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponBuilder;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponCategory;
 import dev.toma.gunsrpg.common.skills.core.SkillType;
@@ -15,7 +19,7 @@ import net.minecraft.util.ResourceLocation;
 public class R45Item extends GunItem {
 
     private static final ResourceLocation RELOAD = GunsRPG.makeResource("r45/reload");
-    private static final ResourceLocation RELOAD_BULLET = GunsRPG.makeResource("r45/reload_bullet");
+    private static final ResourceLocation RELOAD_BULLET = GunsRPG.makeResource("r45/load_bullet");
 
     public R45Item(String name) {
         super(name, new Properties().setISTER(() -> R45Renderer::new));
@@ -24,10 +28,25 @@ public class R45Item extends GunItem {
     @Override
     public void initializeWeapon(WeaponBuilder builder) {
         builder
-                .category(WeaponCategory.PISTOL)
                 .config(ModConfig.weaponConfig.r45)
-                .ammo()
+                .ammo(WeaponCategory.PISTOL)
+                    .define(AmmoMaterials.WOOD, 0)
                 .build();
+    }
+
+    @Override
+    public int getMaxAmmo(IAttributeProvider provider) {
+        return 6;
+    }
+
+    @Override
+    public int getReloadTime(IAttributeProvider provider) {
+        return 17;
+    }
+
+    @Override
+    public IReloadManager getReloadManager(PlayerEntity player) {
+        return ReloadManagers.singleBulletLoading(26, player, this, player.getMainHandItem(), RELOAD_BULLET);
     }
 
     @Override
