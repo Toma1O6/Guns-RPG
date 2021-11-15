57f8908f3247087d2eded53b7632b3abda9d32c2
Toma <novotny.tom96@gmail.com>
2021-11-16 00:07:18 +0100

    Thompson Ammo configuration

--- src/main/java/dev/toma/gunsrpg/common/item/guns/ThompsonItem.java
+++ src/main/java/dev/toma/gunsrpg/common/item/guns/ThompsonItem.java
@@ -4,6 +4,7 @@ import dev.toma.gunsrpg.GunsRPG;
 import dev.toma.gunsrpg.client.render.RenderConfigs;
 import dev.toma.gunsrpg.client.render.item.ThompsonRenderer;
 import dev.toma.gunsrpg.common.init.Skills;
+import dev.toma.gunsrpg.common.item.guns.ammo.AmmoMaterials;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponBuilder;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponCategory;
 import dev.toma.gunsrpg.common.skills.core.SkillType;
@@ -31,6 +32,17 @@ public class ThompsonItem extends GunItem {
                 .category(WeaponCategory.SMG)
                 .config(ModConfig.weaponConfig.thompson)
                 .ammo()
+                    .define(AmmoMaterials.WOOD, 0)
+                    .define(AmmoMaterials.STONE, 2)
+                    .define(AmmoMaterials.IRON, 4)
+                    .define(AmmoMaterials.LAPIS, 4)
+                    .define(AmmoMaterials.GOLD, 6)
+                    .define(AmmoMaterials.REDSTONE, 6)
+                    .define(AmmoMaterials.DIAMOND, 8)
+                    .define(AmmoMaterials.QUARTZ, 8)
+                    .define(AmmoMaterials.EMERALD, 10)
+                    .define(AmmoMaterials.AMETHYST, 12)
+                    .define(AmmoMaterials.NETHERITE, 14)
                 .build();
     }
 
