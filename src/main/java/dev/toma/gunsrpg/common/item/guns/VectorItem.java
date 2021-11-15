ffd3f67aeda90107f10e3f77379992dcd581e8e8
Toma <novotny.tom96@gmail.com>
2021-11-16 00:13:03 +0100

    Vector Ammo configuration

--- src/main/java/dev/toma/gunsrpg/common/item/guns/VectorItem.java
+++ src/main/java/dev/toma/gunsrpg/common/item/guns/VectorItem.java
@@ -4,6 +4,7 @@ import dev.toma.gunsrpg.GunsRPG;
 import dev.toma.gunsrpg.client.render.RenderConfigs;
 import dev.toma.gunsrpg.client.render.item.VectorRenderer;
 import dev.toma.gunsrpg.common.init.Skills;
+import dev.toma.gunsrpg.common.item.guns.ammo.AmmoMaterials;
 import dev.toma.gunsrpg.common.item.guns.ammo.AmmoType;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponBuilder;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponCategory;
@@ -32,6 +33,17 @@ public class VectorItem extends GunItem {
                 .config(ModConfig.weaponConfig.vector)
                 .caliber(AmmoType.AMMO_9MM)
                 .ammo(WeaponCategory.SMG)
+                    .define(AmmoMaterials.WOOD, 0)
+                    .define(AmmoMaterials.STONE, 1)
+                    .define(AmmoMaterials.IRON, 2)
+                    .define(AmmoMaterials.LAPIS, 2)
+                    .define(AmmoMaterials.GOLD, 3)
+                    .define(AmmoMaterials.REDSTONE, 3)
+                    .define(AmmoMaterials.DIAMOND, 5)
+                    .define(AmmoMaterials.QUARTZ, 5)
+                    .define(AmmoMaterials.EMERALD, 6)
+                    .define(AmmoMaterials.AMETHYST, 7)
+                    .define(AmmoMaterials.NETHERITE, 9)
                 .build();
     }
 
