88468614beb80223e5bc28ba02f358c0b4af203b
Toma <novotny.tom96@gmail.com>
2021-11-16 00:04:16 +0100

    Desert Eagle Ammo configuration

--- src/main/java/dev/toma/gunsrpg/common/item/guns/DesertEagleItem.java
+++ src/main/java/dev/toma/gunsrpg/common/item/guns/DesertEagleItem.java
@@ -4,6 +4,8 @@ import dev.toma.gunsrpg.GunsRPG;
 import dev.toma.gunsrpg.client.render.RenderConfigs;
 import dev.toma.gunsrpg.client.render.item.DesertEagleRenderer;
 import dev.toma.gunsrpg.common.init.Skills;
+import dev.toma.gunsrpg.common.item.guns.ammo.AmmoMaterials;
+import dev.toma.gunsrpg.common.item.guns.ammo.AmmoType;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponBuilder;
 import dev.toma.gunsrpg.common.item.guns.setup.WeaponCategory;
 import dev.toma.gunsrpg.common.skills.core.SkillType;
@@ -24,8 +26,20 @@ public class DesertEagleItem extends GunItem {
     public void initializeWeapon(WeaponBuilder builder) {
         builder
                 .category(WeaponCategory.PISTOL)
+                .caliber(AmmoType.AMMO_762MM)
                 .config(ModConfig.weaponConfig.desertEagle)
                 .ammo()
+                    .define(AmmoMaterials.WOOD, 0)
+                    .define(AmmoMaterials.STONE, 3)
+                    .define(AmmoMaterials.IRON, 6)
+                    .define(AmmoMaterials.LAPIS, 6)
+                    .define(AmmoMaterials.GOLD, 9)
+                    .define(AmmoMaterials.REDSTONE, 9)
+                    .define(AmmoMaterials.DIAMOND, 13)
+                    .define(AmmoMaterials.QUARTZ, 13)
+                    .define(AmmoMaterials.EMERALD, 16)
+                    .define(AmmoMaterials.AMETHYST, 20)
+                    .define(AmmoMaterials.NETHERITE, 23)
                 .build();
     }
 
