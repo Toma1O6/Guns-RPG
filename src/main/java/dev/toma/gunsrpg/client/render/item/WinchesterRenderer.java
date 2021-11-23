66f59670e089649b3023bafed6f1754b9c81959c
Toma <novotny.tom96@gmail.com>
2021-11-23 13:49:56 +0100

    Winchester renderer

--- src/main/java/dev/toma/gunsrpg/client/render/item/WinchesterRenderer.java
+++ src/main/java/dev/toma/gunsrpg/client/render/item/WinchesterRenderer.java
@@ -3,9 +3,16 @@ package dev.toma.gunsrpg.client.render.item;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import dev.toma.gunsrpg.client.model.WeaponModels;
 import dev.toma.gunsrpg.client.model.weapon.AbstractWeaponModel;
-import dev.toma.gunsrpg.client.render.item.AbstractWeaponRenderer;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 
+
+/*
+//Hand render config
+//right
+
+//left
+
+ */
 public class WinchesterRenderer extends AbstractWeaponRenderer {
 
     @Override
@@ -17,19 +24,30 @@ public class WinchesterRenderer extends AbstractWeaponRenderer {
     protected void positionModel(MatrixStack stack, ItemCameraTransforms.TransformType transform) {
         switch (transform) {
             case THIRD_PERSON_RIGHT_HAND:
+                stack.translate(-0.2, 0.2, 0.4);
                 break;
             case FIRST_PERSON_RIGHT_HAND:
+                stack.translate(-0.2, 0.25, 0.3);
                 break;
         }
     }
 
     @Override
     protected float scaleForTransform(ItemCameraTransforms.TransformType transform) {
-        return super.scaleForTransform(transform);
+        return 0.25F;
+    }
+
+    @Override
+    protected void scaleModel(MatrixStack matrixStack, ItemCameraTransforms.TransformType transform) {
+        if (transform.firstPerson()) {
+            matrixStack.scale(0.35F, 0.35F, 0.25F);
+            return;
+        }
+        super.scaleModel(matrixStack, transform);
     }
 
     @Override
     protected void transformUI(MatrixStack matrix) {
-
+        matrix.translate(0.4, 0.4, 0.0);
     }
 }
