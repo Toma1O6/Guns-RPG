eda7f605fef1ce778fd015acbda3f5e4ae19ab32
Toma <novotny.tom96@gmail.com>
2022-01-26 12:52:21 +0100

    Fix cycled animation

--- src/main/java/dev/toma/gunsrpg/common/attribute/AttributeModifierFactory.java
+++ src/main/java/dev/toma/gunsrpg/common/attribute/AttributeModifierFactory.java
@@ -18,18 +18,6 @@ public final class AttributeModifierFactory {
     }
 
     /**
-     * Creates new temporary modifier for n ticks
-     * @param uid Unique ID
-     * @param op Operation
-     * @param value Base value
-     * @param ticks Specifies for how many ticks will this modifier be applied
-     * @return New temporary modifier instance
-     */
-    public static ITickableModifier temporary(UUID uid, IModifierOp op, double value, int ticks) {
-        return new TemporaryModifier(uid, op, value, ticks);
-    }
-
-    /**
      * Private constructor, this is just a utility class
      */
     private AttributeModifierFactory() {}
