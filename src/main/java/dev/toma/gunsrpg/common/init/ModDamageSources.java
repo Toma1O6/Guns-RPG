66a9ac8c596a227ed076ca94b990a9900c6bdabf
Toma <novotny.tom96@gmail.com>
2022-06-05 12:48:24 +0200

    Fixed debuff death messages

--- src/main/java/dev/toma/gunsrpg/common/init/ModDamageSources.java
+++ src/main/java/dev/toma/gunsrpg/common/init/ModDamageSources.java
@@ -1,16 +1,19 @@
 package dev.toma.gunsrpg.common.init;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public final class ModDamageSources {
 
-    public static final DamageSource SPIKE_DAMAGE = new DamageSource("spikes").bypassArmor();
-    public static final DamageSource POISON_DAMAGE = new DamageSource("poison").bypassArmor();
-    public static final DamageSource INFECTION_DAMAGE = new DamageSource("infection").bypassArmor();
-    public static final DamageSource BLEED_DAMAGE = new DamageSource("bleeding").bypassArmor();
-    public static final DamageSource FRACTURE_DAMAGE = new DamageSource("fracture").bypassArmor();
+    public static final DamageSource SPIKE_DAMAGE = new SimpleDamageSource("spikes").bypassArmor();
+    public static final DamageSource POISON_DAMAGE = new SimpleDamageSource("poison").bypassArmor();
+    public static final DamageSource INFECTION_DAMAGE = new SimpleDamageSource("infection").bypassArmor();
+    public static final DamageSource BLEED_DAMAGE = new SimpleDamageSource("bleeding").bypassArmor();
+    public static final DamageSource FRACTURE_DAMAGE = new SimpleDamageSource("fracture").bypassArmor();
 
     public static DamageSource dealWeaponDamage(Entity source, Entity indirectSource, ItemStack stack) {
         return new GunDamageSource(source, indirectSource, stack);
@@ -21,4 +24,18 @@ public final class ModDamageSources {
     }
 
     private ModDamageSources() {}
+
+    private static class SimpleDamageSource extends DamageSource {
+
+        private SimpleDamageSource(String messageId) {
+            super(messageId);
+        }
+
+        @Override
+        public ITextComponent getLocalizedDeathMessage(LivingEntity victim) {
+            String messageId = this.getMsgId();
+            String localizationText = "death.attack." + messageId;
+            return new TranslationTextComponent(localizationText, victim.getDisplayName());
+        }
+    }
 }
