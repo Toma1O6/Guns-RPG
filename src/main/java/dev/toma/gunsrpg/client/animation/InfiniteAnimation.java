ab1278aec7528147d4fafbcf78b3e89382cf03b0
Toma <novotny.tom96@gmail.com>
2022-06-09 10:41:39 +0200

    Fixed stuck infinite animation on item switch

--- src/main/java/dev/toma/gunsrpg/client/animation/InfiniteAnimation.java
+++ src/main/java/dev/toma/gunsrpg/client/animation/InfiniteAnimation.java
@@ -2,11 +2,16 @@ package dev.toma.gunsrpg.client.animation;
 
 import lib.toma.animations.api.Animation;
 import lib.toma.animations.api.IKeyframeProvider;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
 
 public class InfiniteAnimation extends Animation {
 
+    private final Item usedItem;
+
     public InfiniteAnimation(IKeyframeProvider provider, int length) {
         super(provider, length);
+        this.usedItem = Minecraft.getInstance().player.getMainHandItem().getItem();
     }
 
     @Override
@@ -16,6 +21,7 @@ public class InfiniteAnimation extends Animation {
 
     @Override
     public boolean hasFinished() {
-        return false;
+        Item item = Minecraft.getInstance().player.getMainHandItem().getItem();
+        return item != usedItem;
     }
 }
