f23bfb34dcc98efb162fbcdeda5a5d4412a88d66
Toma <novotny.tom96@gmail.com>
2022-06-09 10:24:11 +0200

    Custom sensor fallback to default behaviour

--- src/main/java/dev/toma/gunsrpg/ai/QuestPlayerSensor.java
+++ src/main/java/dev/toma/gunsrpg/ai/QuestPlayerSensor.java
@@ -6,12 +6,11 @@ import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.ai.brain.memory.MemoryModuleType;
 import net.minecraft.entity.ai.brain.sensor.Sensor;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.EntityPredicates;
 import net.minecraft.world.server.ServerWorld;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class QuestPlayerSensor extends Sensor<LivingEntity> {
 
@@ -26,7 +25,15 @@ public class QuestPlayerSensor extends Sensor<LivingEntity> {
     @Override
     protected void doTick(ServerWorld world, LivingEntity entity) {
         Brain<?> brain = entity.getBrain();
-        if (target == null || target.isDeadOrDying() || target.isSpectator() || target.isCreative()) return;
+        if (target == null || target.isDeadOrDying() || target.isSpectator() || target.isCreative()) {
+            List<PlayerEntity> list = world.players().stream().filter(EntityPredicates.NO_SPECTATORS).filter((player) -> entity.closerThan(player, 16.0D)).sorted(Comparator.comparingDouble(entity::distanceToSqr)).collect(Collectors.toList());
+            brain.setMemory(MemoryModuleType.NEAREST_PLAYERS, list);
+            List<PlayerEntity> list1 = list.stream().filter((player) -> isEntityTargetable(entity, player)).collect(Collectors.toList());
+            brain.setMemory(MemoryModuleType.NEAREST_VISIBLE_PLAYER, list1.isEmpty() ? null : list1.get(0));
+            Optional<PlayerEntity> optional = list1.stream().filter(EntityPredicates.ATTACK_ALLOWED).findFirst();
+            brain.setMemory(MemoryModuleType.NEAREST_VISIBLE_TARGETABLE_PLAYER, optional);
+            return;
+        }
         brain.setMemory(MemoryModuleType.ANGRY_AT, target.getUUID());
         brain.setMemory(MemoryModuleType.NEAREST_PLAYERS, targetList);
         brain.setMemory(MemoryModuleType.NEAREST_VISIBLE_PLAYER, target);
