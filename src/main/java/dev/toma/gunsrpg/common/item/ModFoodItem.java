b48a802939cd565b58195c61ee47fd99363c45c6
Toma <novotny.tom96@gmail.com>
2022-10-10 14:35:48 +0200

    Better food description

--- src/main/java/dev/toma/gunsrpg/common/item/ModFoodItem.java
+++ src/main/java/dev/toma/gunsrpg/common/item/ModFoodItem.java
@@ -1,35 +1,139 @@
 package dev.toma.gunsrpg.common.item;
 
+import com.mojang.datafixers.util.Pair;
 import dev.toma.gunsrpg.ModTabs;
+import dev.toma.gunsrpg.util.SkillUtil;
+import dev.toma.gunsrpg.util.helper.NumberHelper;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Food;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
 
 public class ModFoodItem extends BaseItem {
 
-    private Consumer<PlayerEntity> onEaten;
+    private final List<IFoodBuff> foodBuffs = new ArrayList<>();
 
     public ModFoodItem(String name, Food food) {
         super(name, new Properties().tab(ModTabs.ITEM_TAB).food(Objects.requireNonNull(food)));
+        for (Pair<EffectInstance, Float> pair : food.getEffects()) {
+            this.addCustomBuff(new PotionBuff(pair.getFirst()));
+        }
     }
 
-    public ModFoodItem buff(Consumer<PlayerEntity> consumer) {
-        this.onEaten = consumer;
+    public ModFoodItem heal(int amount, boolean withAttributes) {
+        return this.addCustomBuff(new HealthBuff(amount, withAttributes));
+    }
+
+    public ModFoodItem heal(int amount) {
+        return this.heal(amount, true);
+    }
+
+    public ModFoodItem addCustomBuff(IFoodBuff foodBuff) {
+        this.foodBuffs.add(foodBuff);
+        this.foodBuffs.sort(Comparator.comparingInt(IFoodBuff::orderIndex));
         return this;
     }
 
     @Override
     public ItemStack finishUsingItem(ItemStack stack, World world, LivingEntity entity) {
         if (isEdible()) {
-            if (onEaten != null && entity instanceof PlayerEntity && !world.isClientSide)
-                onEaten.accept((PlayerEntity) entity);
+            if (entity instanceof PlayerEntity && !world.isClientSide) {
+                PlayerEntity player = (PlayerEntity) entity;
+                for (IFoodBuff foodBuff : foodBuffs) {
+                    foodBuff.accept(player);
+                }
+            }
             return entity.eat(world, stack);
         }
         return stack;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable World world, List<ITextComponent> components, ITooltipFlag flag) {
+        for (IFoodBuff foodBuff : foodBuffs) {
+            components.add(foodBuff.getTooltip());
+        }
+    }
+
+    public interface IFoodBuff extends Consumer<PlayerEntity> {
+
+        ITextComponent getTooltip();
+
+        default int orderIndex() {
+            return 0;
+        }
+    }
+
+    private static class HealthBuff implements IFoodBuff {
+
+        private final int amount;
+        private final boolean applyAttributes;
+        private final ITextComponent tooltip;
+
+        private HealthBuff(int amount, boolean applyAttributes) {
+            this.amount = amount;
+            this.applyAttributes = applyAttributes;
+            ITextComponent hpComponent = new StringTextComponent(String.valueOf(amount)).withStyle(TextFormatting.GREEN);
+            this.tooltip = new TranslationTextComponent("item.gunsrpg.food_buff.health", hpComponent).withStyle(TextFormatting.GRAY);
+        }
+
+        @Override
+        public void accept(PlayerEntity player) {
+            if (applyAttributes) {
+                SkillUtil.heal(player, amount);
+            } else {
+                player.heal(amount);
+            }
+        }
+
+        @Override
+        public ITextComponent getTooltip() {
+            return tooltip;
+        }
+    }
+
+    private static class PotionBuff implements IFoodBuff {
+
+        private final ITextComponent tooltip;
+
+        private PotionBuff(EffectInstance instance) {
+            Effect effect = instance.getEffect();
+            int amplifierVal = instance.getAmplifier() + 1;
+            ITextComponent effectComponent = new StringTextComponent(effect.getDisplayName().getString() + " " + NumberHelper.toRomanLiteral(amplifierVal)).withStyle(TextFormatting.YELLOW);
+            ITextComponent timeComponent = new StringTextComponent(String.valueOf(instance.getDuration() / 20)).withStyle(TextFormatting.GREEN);
+            this.tooltip = new TranslationTextComponent("item.gunsrpg.food_buff.effect", effectComponent, timeComponent).withStyle(TextFormatting.GRAY);
+        }
+
+        @Override
+        public void accept(PlayerEntity player) {
+        }
+
+        @Override
+        public ITextComponent getTooltip() {
+            return tooltip;
+        }
+
+        @Override
+        public int orderIndex() {
+            return Integer.MAX_VALUE;
+        }
     }
 }
--- src/main/java/dev/toma/gunsrpg/util/helper/NumberHelper.java
+++ src/main/java/dev/toma/gunsrpg/util/helper/NumberHelper.java
@@ -1,6 +1,10 @@
 package dev.toma.gunsrpg.util.helper;
 
+import java.util.TreeMap;
+
 public final class NumberHelper {
+
+    private final static TreeMap<Integer, String> ROMAN_NUMBER_MAP = new TreeMap<>();
 
     public static int parseInt(String string, int fallback) {
         try {
@@ -10,5 +14,30 @@ public final class NumberHelper {
         }
     }
 
-    private NumberHelper() {}
+    public static String toRomanLiteral(int number) {
+        int l = ROMAN_NUMBER_MAP.floorKey(number);
+        if (number == l) {
+            return ROMAN_NUMBER_MAP.get(number);
+        }
+        return ROMAN_NUMBER_MAP.get(l) + toRomanLiteral(number - l);
+    }
+
+    private NumberHelper() {
+    }
+
+    static {
+        ROMAN_NUMBER_MAP.put(1000, "M");
+        ROMAN_NUMBER_MAP.put(900, "CM");
+        ROMAN_NUMBER_MAP.put(500, "D");
+        ROMAN_NUMBER_MAP.put(400, "CD");
+        ROMAN_NUMBER_MAP.put(100, "C");
+        ROMAN_NUMBER_MAP.put(90, "XC");
+        ROMAN_NUMBER_MAP.put(50, "L");
+        ROMAN_NUMBER_MAP.put(40, "XL");
+        ROMAN_NUMBER_MAP.put(10, "X");
+        ROMAN_NUMBER_MAP.put(9, "IX");
+        ROMAN_NUMBER_MAP.put(5, "V");
+        ROMAN_NUMBER_MAP.put(4, "IV");
+        ROMAN_NUMBER_MAP.put(1, "I");
+    }
 }
