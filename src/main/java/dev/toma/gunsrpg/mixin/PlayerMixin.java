4f4e90648ccb43041c1ea5a5b43d23755a7c74f7
Toma <novotny.tom96@gmail.com>
2022-10-11 15:17:08 +0200

    Revert "Revert "Moved most of ASM code to Mixins""
    This reverts commit 84166b101424b7cc459e080095221e62c89cc39a.

---
+++ src/main/java/dev/toma/gunsrpg/mixin/PlayerMixin.java
@@ -0,0 +1,35 @@
+package dev.toma.gunsrpg.mixin;
+
+import dev.toma.gunsrpg.api.common.attribute.IAttributeProvider;
+import dev.toma.gunsrpg.api.common.data.IPlayerData;
+import dev.toma.gunsrpg.common.attribute.Attribs;
+import dev.toma.gunsrpg.common.capability.PlayerData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.LazyOptional;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerMixin extends LivingEntity {
+
+    public PlayerMixin(EntityType<? extends LivingEntity> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "getCurrentItemAttackStrengthDelay", at = @At("HEAD"), cancellable = true)
+    public void gunsrpg_getModifiedAttackStrengthDelay(CallbackInfoReturnable<Float> ci) {
+        PlayerEntity player = (PlayerEntity) (Object) this;
+        double value = player.getAttributeValue(Attributes.ATTACK_SPEED);
+        LazyOptional<IPlayerData> optional = PlayerData.get(player);
+        ci.setReturnValue(optional.map(data -> {
+            IAttributeProvider provider = data.getAttributes();
+            return (float) (value * provider.getAttributeValue(Attribs.MELEE_COOLDOWN));
+        }).orElse((float) value));
+    }
+}
