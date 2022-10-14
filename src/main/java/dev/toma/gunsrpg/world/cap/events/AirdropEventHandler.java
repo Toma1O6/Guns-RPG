1ce9c3c26993bb888e152c87d4f3b2e32043da9e
Toma <novotny.tom96@gmail.com>
2022-10-14 14:30:52 +0200

    Fixed issue with airdrop spawning

--- src/main/java/dev/toma/gunsrpg/world/cap/events/AirdropEventHandler.java
+++ src/main/java/dev/toma/gunsrpg/world/cap/events/AirdropEventHandler.java
@@ -1,5 +1,6 @@
 package dev.toma.gunsrpg.world.cap.events;
 
+import dev.toma.gunsrpg.GunsRPG;
 import dev.toma.gunsrpg.api.common.data.IWorldEventHandler;
 import dev.toma.gunsrpg.common.entity.AirdropEntity;
 import dev.toma.gunsrpg.common.init.ModSounds;
@@ -44,16 +45,18 @@ public class AirdropEventHandler implements IWorldEventHandler {
         PlayerEntity player = ModUtils.getRandomListElement(playerList, random);
         if (player == null) return;
         int maxDist = 35;
-        int x = randomInRange(maxDist);
-        int z = randomInRange(maxDist);
+        int x = (int) (player.getX() + randomInRange(maxDist));
+        int z = (int) (player.getZ() + randomInRange(maxDist));
         int y = world.getHeight(Heightmap.Type.WORLD_SURFACE, x, z) + 100;
         BlockPos pos = new BlockPos(player.getX() + x, y, player.getZ() + z);
-        if (world.isEmptyBlock(pos)) {
-            AirdropEntity entity = new AirdropEntity(world);
-            entity.setPos(pos.getX(), pos.getY(), pos.getZ());
-            world.playSound(null, pos.getX(), player.getY(), pos.getZ(), ModSounds.PLANE_FLY_BY, SoundCategory.MASTER, 15.0F, 1.0F);
-            world.addFreshEntity(entity);
+        if (!world.isEmptyBlock(pos)) {
+            GunsRPG.log.error("Airdrop spawning failed due to position being obstructed at {}", pos);
+            return;
         }
+        AirdropEntity entity = new AirdropEntity(world);
+        entity.setPos(pos.getX(), pos.getY(), pos.getZ());
+        world.playSound(null, pos.getX(), player.getY(), pos.getZ(), ModSounds.PLANE_FLY_BY, SoundCategory.MASTER, 15.0F, 1.0F);
+        world.addFreshEntity(entity);
         float anotherAirdropChance = 0.005F;
         if (random.nextFloat() < anotherAirdropChance) {
             addAirdrop(world, playerList);
