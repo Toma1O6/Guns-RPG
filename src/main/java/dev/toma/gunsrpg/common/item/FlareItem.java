fc3ef7c7816a955e73e074a0aa1dfef9d16b4baf
Toma <novotny.tom96@gmail.com>
2023-02-10 23:59:00 +0100

    Add use cooldown to flares

--- src/main/java/dev/toma/gunsrpg/common/item/FlareItem.java
+++ src/main/java/dev/toma/gunsrpg/common/item/FlareItem.java
@@ -3,11 +3,9 @@ package dev.toma.gunsrpg.common.item;
 import dev.toma.gunsrpg.ModTabs;
 import dev.toma.gunsrpg.common.entity.FlareEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
 
 public class FlareItem extends BaseItem {
@@ -20,12 +18,17 @@ public class FlareItem extends BaseItem {
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getItemInHand(hand);
         if (!world.isClientSide) {
-            FlareEntity entity = new FlareEntity(world, player);
-            entity.setThrownByPlayer(player);
-            world.addFreshEntity(entity);
-            world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundCategory.MASTER, 1.0F, 1.0F);
-            if (!player.isCreative()) {
-                stack.shrink(1);
+            CooldownTracker tracker = player.getCooldowns();
+            Item item = stack.getItem();
+            if (!tracker.isOnCooldown(item)) {
+                FlareEntity entity = new FlareEntity(world, player);
+                entity.setThrownByPlayer(player);
+                world.addFreshEntity(entity);
+                world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundCategory.MASTER, 1.0F, 1.0F);
+                if (!player.isCreative()) {
+                    stack.shrink(1);
+                }
+                tracker.addCooldown(item, 30);
             }
         }
         return ActionResult.pass(stack);
