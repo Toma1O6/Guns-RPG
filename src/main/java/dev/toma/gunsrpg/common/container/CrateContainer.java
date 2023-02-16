ced0d35fd4dd3543377cc75627c2f743259829e8
Toma <novotny.tom96@gmail.com>
2023-02-16 11:10:07 +0100

    Airdrop/Military crates are now destroyed instantly once their inventory is emptied

--- src/main/java/dev/toma/gunsrpg/common/container/CrateContainer.java
+++ src/main/java/dev/toma/gunsrpg/common/container/CrateContainer.java
@@ -3,8 +3,11 @@ package dev.toma.gunsrpg.common.container;
 import dev.toma.gunsrpg.common.init.ModContainers;
 import dev.toma.gunsrpg.common.tileentity.ILootGenerator;
 import dev.toma.gunsrpg.common.tileentity.InventoryTileEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.items.SlotItemHandler;
 
 public class CrateContainer<T extends InventoryTileEntity & ILootGenerator> extends AbstractModContainer<T> {
@@ -21,5 +24,16 @@ public class CrateContainer<T extends InventoryTileEntity & ILootGenerator> exte
 
     public CrateContainer(int windowID, PlayerInventory inventory, PacketBuffer buffer) {
         this(windowID, inventory, readTileEntity(buffer, inventory));
+    }
+
+    @Override
+    public void removed(PlayerEntity player) {
+        super.removed(player);
+        T inventory = this.getTileEntity();
+        World world = player.level;
+        if (!world.isClientSide && inventory.isEmpty()) {
+            BlockPos pos = inventory.getBlockPos();
+            world.destroyBlock(pos, true);
+        }
     }
 }
