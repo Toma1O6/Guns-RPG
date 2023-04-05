eb930caf3fd46b97740dadec126d7368a3458f13
Toma <novotny.tom96@gmail.com>
2023-04-05 11:41:19 +0200

    Fix #29 - TaC incompatibility

--- src/main/java/dev/toma/gunsrpg/mixin/LivingEntityMixin.java
+++ src/main/java/dev/toma/gunsrpg/mixin/LivingEntityMixin.java
@@ -10,27 +10,34 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+
+    private DamageSource lastDamageSource;
 
     public LivingEntityMixin(EntityType<?> p_i48580_1_, World p_i48580_2_) {
         super(p_i48580_1_, p_i48580_2_);
     }
 
     @Inject(method = "canSee", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;clip(Lnet/minecraft/util/math/RayTraceContext;)Lnet/minecraft/util/math/BlockRayTraceResult;"), cancellable = true)
-    public void gunsrpg_canEntitySee(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+    private void gunsrpg_canEntitySee(Entity entity, CallbackInfoReturnable<Boolean> cir) {
         if (WorldData.isBloodMoon(entity.level)) {
             cir.setReturnValue(true);
         }
     }
 
-    @Redirect(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;knockback(FDD)V"))
-    public void gunsrpg_disableKnockback(LivingEntity instance, float knockbackAmount, double x, double z, DamageSource source) {
-        if (!(source instanceof WeaponDamageSource)) {
-            instance.knockback(knockbackAmount, x, z);
+    @Inject(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;knockback(FDD)V", shift = At.Shift.BEFORE))
+    private void gunsrpg_captureDamageSourceForKnockback(DamageSource damageSource, float amount, CallbackInfoReturnable<Boolean> cir) {
+        lastDamageSource = damageSource;
+    }
+
+    @Inject(method = "knockback", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraftforge/event/entity/living/LivingKnockBackEvent;getStrength()F"), cancellable = true)
+    private void gunsrpg_disableKnockback(float strength, double x, double z, CallbackInfo ci) {
+        if (lastDamageSource instanceof WeaponDamageSource) {
+            ci.cancel();
         }
     }
 }
